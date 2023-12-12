13ac90642d2294574cc7d416583b460673d6f1c8
adann77 <syahdan22ti@mahasiswa.pcr.ac.id>
2023-12-12 15:45:40 +0700

    ini adalah project saya

---
+++ application/views/layout/v_footer_frontend.php
@@ -0,0 +1,25 @@
+</div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; Dudans Busana </strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+</body>
+</html>
